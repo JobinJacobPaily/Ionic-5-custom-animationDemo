be128a462af800a80d73510c0c566b4c63bdacc9
jobinjacobpaily <jobinjacobpaily@gmail.com>
2020-08-17 20:56:21 +0530

    Animation added for listing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,17 +2,20 @@ import { Component, AfterViewInit, ViewChild, ElementRef, AfterViewChecked } fro
 import { AnimationController,Animation } from '@ionic/angular'; 
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements AfterViewInit{
-
+ 
+  //@ViewChild('customer',{static:true}) customer :ElementRef;
   anim:Animation;
-
+  modalvalue=false;
+  
   @ViewChild('sqaure',{static:true}) square :ElementRef;
-
+ 
   constructor(
     public animationController :  AnimationController,
     public router : Router,
@@ -22,7 +25,7 @@ export class HomePage implements AfterViewInit{
    ngAfterViewInit() {
 
    
-    this.anim = this.animationController.create('animi'); 
+     this.anim = this.animationController.create('animi'); 
     this.anim.addElement(this.square.nativeElement)
     .duration(2000)
     .iterations(Infinity)
@@ -33,9 +36,21 @@ export class HomePage implements AfterViewInit{
      { offset : .75 , transform:'translateY(100px) translateX(-100px)'},
      { offset : 1 , transform:'translateY(0px)'},
     ]);
-    this.anim.play();
+    this.anim.play(); 
+   /*  const listanimi =this.animationController.create('animi');
+    listanimi.addElement(this.customer.nativeElement)
+   .duration(1500)
+   .keyframes([
+     {offset:0,transform:'translateX(100px)'},
+     {offset:.50,transform:'translateX(50px)'},
+     {offset: 1 ,transform:'translateX(0px)'}
+   ])
+   .easing('ease-out')
+   .fromTo('opacity',0,1);
+   listanimi.play(); */
    
   } 
+ 
   stop()
    {
      this.anim.stop();
--- src/app/second/second.page.ts
+++ src/app/second/second.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-second',
@@ -8,7 +9,60 @@ import { Router } from '@angular/router';
 })
 export class SecondPage implements OnInit {
 
+  customersListDB = [
+ 
+    {'index':0,'customer':'soman','balance':1200},
+    {'index':1,'customer':'soman','balance':1200},
+    {'index':2,'customer':'soman','balance':1200},
+    {'index':3,'customer':'soman','balance':1200},
+    {'index':4,'customer':'soman','balance':1200},
+    {'index':5,'customer':'soman','balance':1200},
+    {'index':6,'customer':'soman','balance':1200},
+    {'index':7,'customer':'soman','balance':1200},
+    { 'index':8,'customer':'soman','balance':1200},
+    {'index':9,'customer':'soman','balance':1200},
+    {'index':10,'customer':'soman','balance':1200},
+    {'index':11,'customer':'soman','balance':1200},
+    {'index':0,'customer':'soman','balance':1200},
+    {'index':1,'customer':'soman','balance':1200},
+    {'index':2,'customer':'soman','balance':1200},
+    {'index':3,'customer':'soman','balance':1200},
+    {'index':4,'customer':'soman','balance':1200},
+    {'index':5,'customer':'soman','balance':1200},
+    {'index':6,'customer':'soman','balance':1200},
+    {'index':7,'customer':'soman','balance':1200},
+    { 'index':8,'customer':'soman','balance':1200},
+    {'index':9,'customer':'soman','balance':1200},
+    {'index':10,'customer':'soman','balance':1200},
+    {'index':11,'customer':'soman','balance':1200},
+    {'index':0,'customer':'soman','balance':1200},
+    {'index':1,'customer':'soman','balance':1200},
+    {'index':2,'customer':'soman','balance':1200},
+    {'index':3,'customer':'soman','balance':1200},
+    {'index':4,'customer':'soman','balance':1200},
+    {'index':5,'customer':'soman','balance':1200},
+    {'index':6,'customer':'soman','balance':1200},
+    {'index':7,'customer':'soman','balance':1200},
+    { 'index':8,'customer':'soman','balance':1200},
+    {'index':9,'customer':'soman','balance':1200},
+    {'index':10,'customer':'soman','balance':1200},
+    {'index':11,'customer':'soman','balance':1200},
+    {'index':0,'customer':'soman','balance':1200},
+    {'index':1,'customer':'soman','balance':1200},
+    {'index':2,'customer':'soman','balance':1200},
+    {'index':3,'customer':'soman','balance':1200},
+    {'index':4,'customer':'soman','balance':1200},
+    {'index':5,'customer':'soman','balance':1200},
+    {'index':6,'customer':'soman','balance':1200},
+    {'index':7,'customer':'soman','balance':1200},
+    { 'index':8,'customer':'soman','balance':1200},
+    {'index':9,'customer':'soman','balance':1200},
+    {'index':10,'customer':'soman','balance':1200},
+    {'index':11,'customer':'soman','balance':1200}
+  ]
+
   constructor(
+    public sanitizer:DomSanitizer,
     public router:Router
   ) { }
 
